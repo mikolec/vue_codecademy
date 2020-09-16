66d9be055d27f8d7e8b0b0f2a8402a2fb5eb37e3
Mikołaj Lechtański <mikolec@gmail.com>
2020-09-16 18:55:28 +0200

    Vue forms

---
+++ docs/01_introduction_revue.md
@@ -0,0 +1,9 @@
+Whew! This has been quite a whirlwind tour of Vue.js. We hope you enjoyed getting to try some of the awesome features Vue has to offer. If you were overwhelmed by all of this new content — don’t fret! We will be teaching all of these skills (and many others) in much more depth in future Vue lessons. Let’s summarize some of the awesome things Vue can do:
+
+- Make front-end code quicker to write — loading Vue in one line, mustache templates, built-in directives, and re-usable components make Vue easy to read and easy to write
+- Make front-end code easier to modify and fix — using readable mustache templates, using built-in heavily-tested directives, and consolidating repetitive code into components make Vue code less error-prone and easier to fix when errors do occur
+- Make front-end apps fast and responsive — Vue’s use of a Virtual DOM makes site updates happen only when they need to and incredibly fast when they do
+
+You might still be asking yourself at this point — is Vue right for me? Should I be learning Angular or React instead? The answer is that there is no right answer! All of these front-end frameworks work incredibly well and are getting better every year. Each framework has its own syntax, but many of the skills you acquire will be transferable between frameworks. React also uses a virtual DOM. Angular also uses directives. Both use mustache-like templates and components. Learn the framework that is most exciting to you. All that matters is that you never get stuck in your learning journey.
+
+We hope you enjoyed this overview of Vue.js. We’re excited to teach you more!
--- js/app.js
+++ js/app.js
@@ -1,13 +1,75 @@
 const app = new Vue({
   el: "#app",
   data: {
-    username: "CoderInTraining",
-    newTweet: "",
-    tweets: [
-      "Started learning to code today. Wish me luck!",
-      "Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.",
-      "Today I start learning Vue. I got this.",
-    ],
-    bio: "Excited future front-end  engineer.",
+    firstName: "",
+    lastName: "",
+    email: "",
+    ticketQuantity: 1,
+    ticketType: "general",
+    referrals: [],
+    specialRequests: "",
+    purchaseAgreementSigned: false,
+  },
+  computed: {
+    fullName: {
+      get: function () {
+        if (this.firstName && this.lastName) {
+          return this.firstName + " " + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      set: function (newFullName) {
+        const names = newFullName.split(" ");
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+
+        if (names.length <= 1) {
+          this.firstName = names[0] || "";
+          this.lastName = "";
+        }
+      },
+    },
+    ticketDescription: function () {
+      let readableTicketType = "General Admission";
+      if (this.ticketType === "vip") {
+        readableTicketType = "VIP";
+      }
+
+      let ticketPluralization = "tickets";
+      if (this.ticketQuantity === 1) {
+        ticketPluralization = "ticket";
+      }
+
+      return this.ticketQuantity + " " + readableTicketType + " " + ticketPluralization;
+    },
+    formIsValid: function () {
+      return this.firstName && this.lastName && this.email && this.purchaseAgreementSigned;
+    },
+  },
+  watch: {
+    specialRequests: function (newRequests, oldRequests) {
+      if (
+        newRequests.toLowerCase().includes("meet and greet") ||
+        newRequests.toLowerCase().includes("meet-and-greet")
+      ) {
+        this.ticketType = "vip";
+      }
+    },
+  },
+  methods: {
+    resetFields: function () {
+      this.firstName = "";
+      this.lastName = "";
+      this.email = "";
+      this.ticketQuantity = 1;
+      this.ticketType = "general";
+      this.referrals = [];
+      this.specialRequests = "";
+      this.purchaseAgreementSigned = false;
+    },
   },
 });
